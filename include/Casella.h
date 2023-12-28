3a0b8756a356587e6f157027c73d4fa9c2b387de
Fr33st <150927075+Fr33st@users.noreply.github.com>
2023-12-28 23:29:26 +0100

    added casella

---
+++ include/Casella.h
@@ -0,0 +1,14 @@
+#ifndef CASELLA
+#define CASELLA
+
+class Casella{
+//pensavo come attributoo di casella mettere solo laterale e fare tre classi derivate lusso economica e standard con i diversi attributi del caso
+    private:
+    
+    
+    public:
+
+
+};
+
+#endif // CASELLA
