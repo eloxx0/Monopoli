7906e1d29c71158390e7c614fc41475f5b4f716b
Fr33st <150927075+Fr33st@users.noreply.github.com>
2024-01-04 17:04:16 +0100

    need to be fixed

--- src/Casella.cpp
+++ src/Casella.cpp
@@ -1,3 +1,10 @@
 #include "../include/Casella.h"
 
-Casella::Casella():propriety{0}, house{false}, hotel{false}{}
+Casella::Casella():status{0}, player{nullptr}, house{false}, hotel{false}{}
+
+
+std::ostream& operator<<(std::ostream& out, const Casella& x){//sistemare
+
+    return out<<x.get_house()<<x.get_hotel();   
+    
+}
