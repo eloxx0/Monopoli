ed3f03f4779280be86bd66b66fc25ebb34ce9461
Fr33st <150927075+Fr33st@users.noreply.github.com>
2024-01-04 10:52:26 +0100

    added changes

--- include/Casella.h
+++ include/Casella.h
@@ -7,10 +7,8 @@ class Casella{
 
     private:
     
-    bool Lusso;
-    bool Economica;
-    bool Standard;
-    //std::string status; 
+    char status; 
+    int propriety;
     //secondo me sono da definire come dei bool, come fossero uno switch
     bool house;
     bool hotel;
@@ -20,16 +18,9 @@ class Casella{
     Casella(); //costruttore di default, casella settata come vuota
     //definire getter lusso/eco/standard tramite status?
     //setter e getter
-    void set_lusso(bool x){
-        Lusso=x;
-    }
     
-    void set_economica(bool x){
-        Economica=x;
-    }
-    
-    void set_standard(bool x){
-        Standard=x;
+    int get_propriety(void){    //ritorna numero player casella oppure 0 se non la possiede nessuno, dovrei avere accesso a player?
+        return propriety;
     }
     
     void set_house(bool x){
@@ -37,7 +28,7 @@ class Casella{
     }
     
     void set_hotel(bool x){ //piccolo check per questa operazione
-        if(house==true) hotel=x;
+        if(house) hotel=x;
         else std::cout<<"Impossibile effettuare questa operazione"<<std::endl;
     }
     
