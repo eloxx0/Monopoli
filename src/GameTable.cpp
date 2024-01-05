2b0db0689964c9ef0d5292386952e53b3c732fad
PdP03 <40737069+PdP03@users.noreply.github.com>
2024-01-05 09:25:02 +0100

    Added gametable population

--- src/GameTable.cpp
+++ src/GameTable.cpp
@@ -1,4 +1,45 @@
 #include "../include/GameTable.h"
 
 GameTable::GameTable(){
+    this-> table=genTable();
 }
+
+Casella* genTable()
+{
+    Casella table[28];
+    int countE{0}, countS{0}, countL{0}, i{0};
+    std::srand(std::time(0));
+    while(i<28)
+    {
+        int rnd_status=1+rand()%3;
+        if(i%7==0) 
+        {
+            table[i].set_status(0);
+        }
+        if(rnd_status==1 && countE<8)
+        {
+            table[i].set_status('E');
+            i++;
+            countE++;
+            break;
+        }
+        if(rnd_status==2 && countS<10)
+        {
+            table[i].set_status('S');
+            i++;
+            countS++;
+            break;
+        }
+        if(rnd_status==3 && countL<6)
+        {
+            table[i].set_status('L');
+            i++;
+            countS++;
+            break;
+        }
+
+    }
+
+    return table;
+}
+
